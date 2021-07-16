62cf8e8673c7b02be6d8c045641b302059de20a0
Brian Griggs <brian@bigmissionmotorsports.com>
2021-07-15 18:34:46 -0600

    Fuel range basic support. #311

---
+++ aspnet-core/common/BigMission.TestHelpers/DateTimeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BigMission.TestHelpers
+{
+    /// <summary>
+    /// DateTime wrapper used for dependency injection
+    /// so unit testing is possible.
+    /// </summary>
+    public class DateTimeHelper : IDateTimeHelper
+    {
+        public DateTime Now => DateTime.Now;
+
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
---
+++ aspnet-core/common/BigMission.TestHelpers/IDateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BigMission.TestHelpers
+{
+    /// <summary>
+    /// DateTime mockable interface to enable unit testing.
+    /// </summary>
+    public interface IDateTimeHelper
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+    }
+}
