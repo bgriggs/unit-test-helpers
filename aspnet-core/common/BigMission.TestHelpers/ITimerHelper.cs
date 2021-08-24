8ee8dd971d5b3c810205b75247db2ace27d5b826
Brian Griggs <brian@bigmissionmotorsports.com>
2021-08-23 22:11:01 -0600

    Fuel stats service refactor to remove dependicies for testing.  #311

---
+++ aspnet-core/common/BigMission.TestHelpers/ITimerHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigMission.TestHelpers
+{
+    public interface ITimerHelper
+    {
+        Timer Create(TimerCallback callback, object? state, TimeSpan dueTime, TimeSpan period);
+        void Dispose();
+        ValueTask DisposeAsync();
+    }
+}
---
+++ aspnet-core/common/BigMission.TestHelpers/TimerHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BigMission.TestHelpers
+{
+    public class TimerHelper : ITimerHelper
+    {
+        private Timer timer;
+
+        public Timer Create(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+        {
+            timer = new Timer(callback, state, dueTime, period);
+            return timer;
+        }
+
+        public void Dispose()
+        {
+            timer.Dispose();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return timer.DisposeAsync();
+        }
+    }
+}
