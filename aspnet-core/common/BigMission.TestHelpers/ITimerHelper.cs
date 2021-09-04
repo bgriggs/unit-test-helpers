efbc8448004d116f13354f68b078670cb475b337
Brian Griggs <brian@bigmissionmotorsports.com>
2021-09-04 10:34:42 -0600

    Fuel range calcs. Fixes #311

--- aspnet-core/common/BigMission.TestHelpers/ITimerHelper.cs
+++ aspnet-core/common/BigMission.TestHelpers/ITimerHelper.cs
@@ -6,7 +6,7 @@ namespace BigMission.TestHelpers
 {
     public interface ITimerHelper
     {
-        Timer Create(TimerCallback callback, object? state, TimeSpan dueTime, TimeSpan period);
+        Timer Create(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period);
         void Dispose();
         ValueTask DisposeAsync();
     }
